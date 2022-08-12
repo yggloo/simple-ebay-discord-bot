b4d9404e1a3cbb0a957f6bd551e1c16618ba1295
yggloo <106108135+yggloo@users.noreply.github.com>
2022-08-11 23:02:12 -0500

    Update viewbot.py

--- viewbot.py
+++ viewbot.py
@@ -2,9 +2,9 @@ import requests
 import threading
 import time
 
-def views(link,views):
+def views(link,num_views):
     #send views requests to the link using threading
-    for i in range(views):
+    for i in range(num_views):
         threading.Thread(target=requests.get, args=(link,)).start()
         time.sleep(.1)
         print(f'{i+1} views sent')
